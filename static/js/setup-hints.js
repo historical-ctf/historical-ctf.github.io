1c97b0c990848cbe8537c59f4a5ae549b85969f8
jamesaevans <jaevans@princeton.edu>
2015-05-08 16:23:36 -0400

    update task 1 copy

--- static/js/setup-hints.js
+++ static/js/setup-hints.js
@@ -22,9 +22,9 @@ function initializeModal() {
 
 var hintIndex = 0;
 var hints = [
-  'The original implmentation of RSA had some critical security flaws. Try searching for "Textbook RSA" and use if you can use what you find to figure out Badguy\s password',
-  'You did well!',
-  'That was great!'
+  'The original implmentation of RSA had some critical security flaws. Try searching for "Textbook RSA" and see if you can use what you find to figure out Badguy\'s password',
+  'You may have realized that the original version of RSA is completely <b>deterministic</b>; the same password encrypted with the same public key will produce the same ciphertext. Try encrypting some common passwords with Badguy\'s public key.',
+  'Click <a>here</a> for a reference solution. Run it to crack Badguy\'s password! Don\'t expect everything in life to be this easy.'
 ];
 
 initializeModal();
