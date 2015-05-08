7b4903c02162f6cd93c1c512acccb7cdc5d5705a
jamesaevans <jaevans@princeton.edu>
2015-05-08 18:00:20 -0400

    task 1 landing page copy

--- scripts/util.py
+++ scripts/util.py
@@ -1,3 +1,4 @@
+# Turn a string into a number for RSA encryption. 
 def serialize(s):
 	ctr = 0
 	i = len(s) - 1
@@ -5,3 +6,5 @@ def serialize(s):
 		ctr += ord(c) ** i
 		i = i - 1
 	return ctr
+
+# Turn a number into a string after RSA decryption. 
