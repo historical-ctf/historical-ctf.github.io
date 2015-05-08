69733aa22d09ad00c78a1027016b7ccaf39724e4
jamesaevans <jaevans@princeton.edu>
2015-05-08 18:23:26 -0400

    display correct text in modal

--- static/js/setup-hints.js
+++ static/js/setup-hints.js
@@ -6,7 +6,12 @@ function initializeModal() {
   if (modalInitializer) {
     // Setup handler
     modalInitializer.onclick = function() {
-      vex.dialog.alert(hints[hintIndex++]);
+      var clicked = $('.help-button').index($(this));
+
+      vex.dialog.alert(hints[clicked]);
+      if (hintIndex == clicked)
+        hintIndex++;
+      $(this).fadeTo("slow", 0.5);
 
       // Make the next hint visible
       initializeModal();
@@ -22,7 +27,7 @@ function initializeModal() {
 
 var hintIndex = 0;
 var hints = [
-  'The original implmentation of RSA had some critical security flaws. Try searching for "Textbook RSA" and see if you can use what you find to figure out Badguy\'s password',
+  'The original implmentation of RSA had some critical security flaws. Try searching for "Textbook RSA" and see if you can use what you find to figure out Badguy\'s password.',
   'You may have realized that the original version of RSA is completely <b>deterministic</b>; the same password encrypted with the same public key will produce the same ciphertext. Try encrypting some common passwords with Badguy\'s public key.',
   'Click <a>here</a> for a reference solution. Run it to crack Badguy\'s password! Don\'t expect everything in life to be this easy.'
 ];
