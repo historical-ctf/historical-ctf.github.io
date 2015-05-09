123aa48f7f745f416311f809cb8b2fb36db30e1e
Charles Marsh <charlie@khanacademy.org>
2015-05-09 10:38:16 -0400

    Congrats modal

--- static/js/setup-hints.js
+++ static/js/setup-hints.js
@@ -8,8 +8,8 @@ function setTask1Hints() {
   hints = [
     'The original implementation of RSA had some critical security flaws. Try searching for "Textbook RSA" and see if you can use what you find to figure out Badguy\'s password.',
     'You may have realized that the original version of RSA is completely <b>deterministic</b>; the same password encrypted with the same public key will produce the same ciphertext. Try encrypting some common passwords with Badguy\'s public key.',
-    'Try <a href="/static/scripts/dictionary.txt">this</a> list of common passwords.',
-    'If you\'re having trouble, check out this <a href="/static/scripts/solution.py">reference solution</a>, which relies on our <a href="/static/scripts/dictionary.txt">list of common passwords</a>. Run it to crack Badguy\'s password! Don\'t expect everything in life to be this easy.'
+    'Try <a target="_blank" href="/static/scripts/dictionary.txt">this</a> list of common passwords.',
+    'If you\'re having trouble, check out this <a target="_blank" href="/static/scripts/solution.py">reference solution</a>, which relies on our <a target="_blank" href="/static/scripts/dictionary.txt">list of common passwords</a>. Run it to crack Badguy\'s password! Don\'t expect everything in life to be this easy.'
   ];
 }
 
@@ -18,8 +18,8 @@ function setTask2Hints() {
   hints = [
     'Try poking around Badguy\'s server to discover a security flaw to exploit. Chat logs are always a good place to start.',
     'You may have noticed that Badguy sent two identical messages using the same modulus and two different exponents. One might even call it a "common" modulus.',
-    'To get you going with the Common Modulus Attack, try this <a href="/static/scripts/skeleton2.py">skeleton code</a> and a new <a href="static/scripts/util2.py">utility file</a>.',
-    'If you\'re having trouble, check out this <a href="/static/scripts/solution2.py">reference solution</a>.'
+    'To get you going with the Common Modulus Attack, try this <a target="_blank" href="/static/scripts/skeleton2.py">skeleton code</a> and a new <a target="_blank" href="static/scripts/util2.py">utility file</a>.',
+    'If you\'re having trouble, check out this <a target="_blank" href="/static/scripts/solution2.py">reference solution</a>.'
   ];
 }
 
