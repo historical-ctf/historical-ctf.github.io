2e3d0ea27153f4ef6c3aac766bdde5619965d23d
Charles Marsh <charlie@khanacademy.org>
2015-05-10 13:01:44 -0400

    Skeleton for task4

---
+++ static/scripts/task4/skeleton.py
@@ -0,0 +1,30 @@
+import util
+
+
+def unblind(signature, r, n):
+    pass
+
+def blind(m, r, e, n):
+    pass
+
+if __name__ == '__main__':
+    # Modulus carries over from previous challenge
+    n =
+
+    # Public exponent
+    e =
+
+    # Choose a large random prime to be the blinding factor
+    r =
+
+    # Sign the initial message for comparison
+    message =
+    m = util.serialize(message)
+
+    # Blind and sign
+    m_prime = blind(m, r, e, n)
+    blind_signature = util.sign(m_prime, d, n)
+    true_signature = unblind(blind_signature, r, n)
+
+    print 'Blinded signature: %d' % blind_signature
+    print 'Unblinded signature: %d' % true_signature
