3b93bedb0fd22299fffe917b20658d51266a3c4a
jamesaevans <jaevans@princeton.edu>
2015-05-10 20:21:07 -0400

    congrats modal

--- static/js/setup-task6.js
+++ static/js/setup-task6.js
@@ -1,13 +1,13 @@
 var debug = true;
 var hasLoggedIn = true;
-var loggedInPrompt = 'bbadguy> ';
+var loggedInPrompt = 'sh> ';
 var isChatting = false;
 var chatPrompt = 'swissco-official: ';
-var chatID = Math.floor(Math.random()*1000);
+var chatID;
 
-function displayTask5Modal() {
+function displayTask6Modal() {
   vex.dialog.alert({
-    message: 'Message about Task 5.',
+    message: 'Congratulations! You\'ve put Badguy behind bars!',
     // Have to include 'vex-theme-plain' or it clobbers the default class
     className: 'vex-theme-plain congratulatory-modal'
   });
@@ -18,8 +18,8 @@ function parseCommand(command, term) {
     var m = command.split(' ')[1];
     $.ajax({
       type: 'POST',
-      url: 'http://localhost:5000/startChat/' + chatID,
-      data: { 'username': m }
+      url: 'https://historical-ctf.herokuapp.com/startChat/' + chatID,
+      data: { 'screename': m }
     }).done(function(result) {
       term.echo(m + ' is online');
       term.set_prompt(chatPrompt);
@@ -35,13 +35,18 @@ function parseCommand(command, term) {
   	var m = command;
 	$.ajax({
       type: 'POST',
-      url: 'http://localhost:5000/chat/' + chatID,
-      data: { 'username': m }
+      url: 'https://historical-ctf.herokuapp.com/chat/' + chatID,
+      data: { 'message': m }
     }).done(function(result) {
       term.echo("searly1965: " + result);
+      term.set_prompt(chatPrompt);
+      if (result.indexOf("boole") > -1) {
+        displayTask6Modal();
+      }
     })
-    .fail(function() {
+    .fail(function(result) {
     	term.error("There was an error connecting with the chat server. Please reload.");
+      console.log(result);
       console.log("Server Error. Shouldn't get here.");
     })
     term.set_prompt('');
@@ -55,8 +60,8 @@ var options = {
     term.set_command('chat {screename}');
     hasLoggedIn = true;
     term.echo('');
-    displayTask5Modal();
   }
 };
 
+chatID = Math.floor(Math.random()*1000),
 setupTask(options);
