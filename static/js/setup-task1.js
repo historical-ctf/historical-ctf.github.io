99c18fefbe1fc5727ad7c2d56d6e434f9f69ee68
Charles Marsh <charlie@khanacademy.org>
2015-05-11 13:38:22 -0400

    Make it clear that user has entered Task 2

--- static/js/setup-task1.js
+++ static/js/setup-task1.js
@@ -4,7 +4,7 @@ var debug = false;
 
 function displayTask1Modal() {
   vex.dialog.alert({
-    message: 'Good job! You\'re now inside Badguy\'s server.<br><br>The key to the previous challenge was that so-called "Textbook RSA" is a deterministic protocol. So if you encrypt the same message twice, you get the same ciphertext.<br><br>Over time, researchers have incorporated randomness into RSA through protocols like <a href="https://en.wikipedia.org/wiki/Optimal_asymmetric_encryption_padding" target="_blank">OAEP encoding</a>.',
+    message: '<b>Welcome to Task 2</b><br><br>Good job! You\'re now inside Badguy\'s server.<br><br>The key to the previous challenge was that so-called "Textbook RSA" is a deterministic protocol. So if you encrypt the same message twice, you get the same ciphertext.<br><br>Over time, researchers have incorporated randomness into RSA through protocols like <a href="https://en.wikipedia.org/wiki/Optimal_asymmetric_encryption_padding" target="_blank">OAEP encoding</a>.',
     // Have to include 'vex-theme-plain' or it clobbers the default class
     className: 'vex-theme-plain congratulatory-modal'
   });
